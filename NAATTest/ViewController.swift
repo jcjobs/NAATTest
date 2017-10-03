6d34b5e755ba1b98e0f8db71a0ecdfa3e506a532
jcjobs <jcarlosp.ceo@gmail.com>
2017-10-03 01:36:17 -0500

    Initial Commit

---
+++ NAATTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NAATTest
+//
+//  Created by Juan Carlos Pérez D. on 10/3/17.
+//  Copyright © 2017 Juan Carlos Pérez D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
